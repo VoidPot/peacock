161e6cd685596686a084a878c79b3dca186ceaca
Zakir Hussain <itsjakirhussain@gmail.com>
2023-06-02 00:30:41 +0530

    home

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,7 @@ import {
 } from "@remix-run/react";
 
 import stylesheet from "~/tailwind.css";
+import BottomNav from "~/components/organisms/bottom-nav";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -19,18 +20,20 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="dark h-full">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body className="h-full">
+      <body className="container m-auto h-full bg-white dark:bg-gray-800">
         <Outlet />
+        <BottomNav />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,20 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        brand: ["BlackEcho", "sans-serif"],
+        sans: ["AeronomicSans", "sans-serif"],
+        serif: ["ArianeCoachella", "serif"],
+        core: ["Poppins", "sans-serif"],
+      }
+    },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
 } satisfies Config;
